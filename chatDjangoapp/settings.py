f842812537682c12a3d9c5c105bac530f72013d4
salemsifeddine <saiftony5@gmail.com>
2022-07-15 16:13:38 +0100

    mmm

--- chatDjangoapp/settings.py
+++ chatDjangoapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "chatappDjango",
-    "rest_framework"
+    "rest_framework",
+    "frontend.apps.FrontendConfig"
 ]
 
 MIDDLEWARE = [
